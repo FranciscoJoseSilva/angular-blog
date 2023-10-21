0415683c1865491f4cb996128e19662ccf3e339d
Francisco Jose <sousa06092001@gmail.com>
2023-10-21 09:23:45 -0400

    content created

---
+++ src/app/data/dataTest.ts
@@ -0,0 +1,26 @@
+export const dataTest = [
+  {
+    "id": "1",
+    "photo": "https://cinemacao.com/wp-content/uploads/2018/02/pantera-negra-017-1130x590.jpg",
+    "title": "Novo filme do Pantera Negra tras mudancas na historia",
+    "decription": "O mais recente filme doa franquia Pantera Negra trouxe uma mudança significativa no visual, e historia dos personagens originais da franquia..."
+  },
+  {
+    "id": "2",
+    "photo": "https://legadodadc.com.br/wp-content/uploads/2022/01/stranger-things-caleb-mclaughlin-static-shock-1050x449-1-752x440-1.jpg.webp",
+    "title": "Filme de Super Chock ganha previsão de estreia para 2023",
+    "decription": ""
+  },
+  {
+    "id": "3",
+    "photo": "https://soulbrasil.com/wp-content/uploads/2023/08/besouro-azul-bruna-marquezine-poster-560x319.jpg",
+    "title": "Saiba tudo sobre o novo filme do Besouro Azul",
+    "decription": ""
+  },
+  {
+    "id": "4",
+    "photo": "https://cinepop.com.br/wp-content/uploads/2023/02/the-flash-1.jpg",
+    "title": "Detalhes que a serie The Flash mostrou, e muitos fãs deixaram passar",
+    "decription": ""
+  }
+]
